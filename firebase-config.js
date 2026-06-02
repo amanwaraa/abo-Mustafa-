4aaba7fbbb11365b0ac0a491aa62fc180db582cd
Maroshop <homworkhhh25@gmail.com>
2026-06-02 09:38:57 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// ملف توافق لمن كان يستدعي firebase-config.js سابقاً
+window.OSKAR_FIREBASE_CONFIG_READY = true;
+window.OSKAR_FIREBASE_LOGIN_KEY = "cashier_units_pro_anwar_28f5e_1052279476923";
+window.OSKAR_FIREBASE_ROOT = "cashier_units_pro_anwar_28f5e_1052279476923";
+window.OSKAR_FIREBASE_FALLBACK_ROOTS = ["cashier_units_pro_anwar_28f5e_1052279476923"];
+window.OSKAR_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAWS8S_GwoeA03SnRUDAW2o3fbHxSI7jLg",
+  authDomain: "anwar-28f5e.firebaseapp.com",
+  databaseURL: "https://anwar-28f5e-default-rtdb.firebaseio.com",
+  projectId: "anwar-28f5e",
+  storageBucket: "anwar-28f5e.firebasestorage.app",
+  messagingSenderId: "1052279476923",
+  appId: "1:1052279476923:web:4fb3d583aad4c39328cf9e",
+  measurementId: "G-6RJDX9J1N7"
+};
+window.OSKAR_FIREBASE_APP_SETTINGS = {
+  firebaseConfig: window.OSKAR_FIREBASE_CONFIG,
+  firebaseRoot: window.OSKAR_FIREBASE_ROOT,
+  fallbackRoots: window.OSKAR_FIREBASE_FALLBACK_ROOTS
+};
